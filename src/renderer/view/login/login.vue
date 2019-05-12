ff1e47f8bf5b3117e7e2a3b624142680ba963725
潘卓然-笔记本Y7000P <parndeedlits@gmail.com>
2019-05-12 14:28:40 +0800

    修正样式-李晨看下这里的修改是怎么改的

--- src/renderer/view/login/login.vue
+++ src/renderer/view/login/login.vue
@@ -1,76 +1,112 @@
 <template>
-<div style="background-color:#aabbcc;">
   <Layout>
-     <Header style="background-color:#e8eaec;height:75px;text-align:center;">
-        <div class="layout-logo" style="float:left;"></div>
-        <div style="float:right;">
-                        <span v-if="maximum" @click="restoreWindow"><Icon type="ios-contract" /></span>
-                        <span v-else @click="restoreWindow"><Icon type="ios-expand"/></span>
-                        <span @click="minWindow"><Icon type="md-remove"/></span>
-                        <span @click="exitApp"><Icon type="md-close"/></span>
-      </div>
-     </Header>
-  <Layout>
-  <Content>
-    <div  style="float:left;text-align:center;width:420px; height:455px; margin:35px;padding:30px;">
-          <h2>扫一扫登录</h2><br><br>
-          <h3 style="font-size:18px;font-weight:normal;">请使用百度网盘扫码登录</h3><br>
-           <div>
-             <img   style="width:230px;height:230px;"  src="../../assets/img/code.png"/>
-           </div>
-           <br>
-           <Button type="primary">刷新二维码</Button>
-    </div>
-  <div style="float:right;width:390px;height:455px;margin-top:35px;padding:30px; margin-right:100px;">
-    <div  style="float:left;"> <span style="font-size:18px">账号密码登录</span></div>
-      <div  style="float:right"><a>短信快捷登录<Icon type="ios-arrow-forward" /></a></div>
-     <br><br><br><br><br>
-     <Form ref="AccountForm" :model="account" :rules="rule">
-        <FormItem prop="user">
-             <Input type="text" size="default" v-model="account.username" placeholder="Username">
-               <Icon type="ios-person-outline" slot="prepend"></Icon>
-             </Input>
-             <br>
-      </FormItem>
-      <FormItem prop="password">
-            <Input type="password" size="default" v-model="account.password" placeholder="Password">
-               <Icon type="ios-lock-outline" slot="prepend" ></Icon>
-           </Input>
-      </FormItem>
-      <FormItem>
-      <CheckboxGroup v-model="check">
-        <Checkbox label="记住密码"></Checkbox>
-        <Checkbox label="自动登录"></Checkbox>
-        <a  style="float:right">忘记密码?</a>
-    </CheckboxGroup>
-    </FormItem>
-      <FormItem>
-           <Button type="primary" @click="handleLogin(account)" :loading="logining" long>登录</Button>
-      </FormItem>
-      </Form>
-      <br><br>
-       <div>
-           <a>注册账号</a>  
-           <div  style="float:right;">
-             <a><img style="width:50px;height:20px;" src="../../assets/img/1.png"></a>
-              <a><img style="width:50px;height:20px;" src="../../assets/img/2.png"></a>
-               <a><img style="width:50px;height:20px;" src="../../assets/img/3.png"></a>
-           </div>
-      </div>
-    </div>
-  </Content>
-</Layout>
-</Layout>
-</div>
+    <Header class="header">
+      <Menu
+        mode="horizontal"
+        active-name="1"
+        theme="light"
+        style="height: 80px;background-color: #EEF0F6;padding-top: 5px;"
+      >
+        <div class="layout-logo"></div>
+        <div class="layout-toolbar-right">
+          <span v-if="maximum" @click="restoreWindow">
+            <Icon type="ios-contract"/>
+          </span>
+          <span v-else @click="restoreWindow">
+            <Icon type="ios-expand"/>
+          </span>
+          <span @click="minWindow">
+            <Icon type="md-remove"/>
+          </span>
+          <span @click="exitApp">
+            <Icon type="md-close"/>
+          </span>
+        </div>
+      </Menu>
+    </Header>
+    <Layout>
+      <Content>
+        <Row :gutter="16">
+          <i-col span="12">
+            <div class="layout-main-left">
+              <h2>扫一扫登录</h2>
+              <br>
+              <h3>请使用百度网盘扫码登录</h3>
+              <br>
+              <div>
+                <img src="../../assets/img/code.png">
+              </div>
+              <br>
+              <Button type="primary">刷新二维码</Button>
+            </div>
+          </i-col>
+          <i-col span="10">
+            <div class="layout-main-right">
+              <div class="float-left">
+                <span>账号密码登录</span>
+              </div>
+              <div class="float-right">
+                <a>
+                  短信快捷登录
+                  <Icon type="ios-arrow-forward"/>
+                </a>
+              </div>
+              <br>
+              <Form ref="AccountForm" :model="account" :rules="rule">
+                <FormItem prop="user">
+                  <Input
+                    type="text"
+                    size="default"
+                    v-model="account.username"
+                    placeholder="Username"
+                  >
+                    <Icon type="ios-person-outline" slot="prepend"></Icon>
+                  </Input>
+                  <br>
+                </FormItem>
+                <FormItem prop="password">
+                  <Input
+                    type="password"
+                    size="default"
+                    v-model="account.password"
+                    placeholder="Password"
+                  >
+                    <Icon type="ios-lock-outline" slot="prepend"></Icon>
+                  </Input>
+                </FormItem>
+                <FormItem>
+                  <CheckboxGroup v-model="check">
+                    <Checkbox label="记住密码"></Checkbox>
+                    <Checkbox label="自动登录"></Checkbox>
+                    <a class="float-right">忘记密码?</a>
+                  </CheckboxGroup>
+                </FormItem>
+                <FormItem>
+                  <Button type="primary" @click="handleLogin(account)" :loading="logining" long>登录</Button>
+                </FormItem>
+              </Form>
+              <br>
+              <div>
+                <a>注册账号</a>
+                <div class="float-right">
+                  <a>
+                    <img src="../../assets/img/1.png">
+                  </a>
+                  <a>
+                    <img src="../../assets/img/2.png">
+                  </a>
+                  <a>
+                    <img src="../../assets/img/3.png">
+                  </a>
+                </div>
+              </div>
+            </div>
+          </i-col>
+        </Row>
+      </Content>
+    </Layout>
+  </Layout>
 </template>
-
-<style>
-.layout-logo {
-        width: 300px;
-        height: 170px;
-        background: url("../../assets/img/logo.png") no-repeat;
-    }
-</style>
 
 <script>
 export default {
@@ -80,9 +116,9 @@ export default {
         username: "admin",
         password: "123456"
       },
-      logining:false,
-      maximum:false,
-      check:['自动登录'],
+      logining: false,
+      maximum: false,
+      check: ["自动登录"],
       rule: {
         username: [
           {
@@ -109,27 +145,100 @@ export default {
   },
   methods: {
     handleLogin(data) {
-      this.$store.dispatch('logins',data).then(res => {
-           if(this.$store.getters.role==='admin'){
-             this.$router.push('/wenjian/fs');
-           }else if(this.$store.getters.role==='user'){
-                 this.$router.push('/index');
-           }else{
-             this.$router.push('/');
-           }
-        }).catch(() => {
-
-        })  
-   },
+      this.$store
+        .dispatch("logins", data)
+        .then(res => {
+          if (this.$store.getters.role === "admin") {
+            this.$router.push("/wenjian/fs");
+          } else if (this.$store.getters.role === "user") {
+            this.$router.push("/index");
+          } else {
+            this.$router.push("/");
+          }
+        })
+        .catch(() => {});
+    },
     exitApp() {
-                this.$electron.ipcRenderer.send('exitApp');
-            },
+      this.$electron.ipcRenderer.send("exitApp");
+    },
     minWindow() {
-                this.$electron.ipcRenderer.send('minWindow');
-            },
+      this.$electron.ipcRenderer.send("minWindow");
+    },
     restoreWindow() {
-                this.$electron.ipcRenderer.send('maximizeWindow');
-            }
+      this.$electron.ipcRenderer.send("maximizeWindow");
+    }
   }
-}
+};
 </script>
+
+<style scoped>
+.ivu-layout {
+  height: 100vh;
+  overflow: hidden;
+}
+.ivu-layout-header {
+  background: "rgb(238, 240, 246)";
+  padding: 0 100px;
+  height: 80px;
+  line-height: 64px;
+}
+
+.header {
+  -webkit-app-region: drag;
+  padding: 0;
+  -webkit-user-select: none;
+  /* font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif; */
+}
+
+.layout-logo {
+  width: 150px;
+  height: 30px;
+  background: url("../../assets/img/logo.png") no-repeat;
+  background-size: 118px;
+  border-radius: 3px;
+  float: left;
+  position: relative;
+  top: 15px;
+  left: 50px;
+  margin-right: 20px;
+}
+
+.layout-toolbar-right {
+  float: right;
+  margin-right: 10px;
+  font-size: 20px;
+  -webkit-app-region: no-drag;
+}
+.layout-main-left {
+  text-align: center;
+  padding: 30px;
+}
+
+.layout-main-left h3 {
+  font-size: 18px;
+  font-weight: normal;
+}
+.layout-main-left img {
+  width: 230px;
+  height: 230px;
+}
+.layout-main-right {
+  margin-top: 35px;
+  padding: 30px;
+}
+
+.layout-main-right span {
+  font-size: 18px;
+}
+.layout-main-right img {
+  width: 50px;
+  height: 20px;
+}
+
+.float-left {
+  float: left;
+}
+.float-right {
+  float: right;
+}
+</style>
